b9362c4fb34e593d0e2ee23a713cab616d155ed4
Ashley Noel Hinton <ashley@canadia.co.nz>
2017-05-01 20:28:52 +1200

    enpackageise

--- R/shirtShuffler.R
+++ R/shirtShuffler.R
@@ -2,11 +2,11 @@
 
 #' Choose which shirt I will wear each weekday
 #'
-#' Raw shirt patterns are found in the \file{rawShirts}
-#' subdirectory. This function randomly selects 5 of the patterns, and
-#' copies files names for each weekday into the \file{order}
-#' directory. Just open the image for each day of the week to find
-#' your shirt surprise.
+#' Raw shirt patterns are found in the directory given by
+#' \code{rawShirts}. This function randomly selects 5 of the patterns,
+#' and copies files names for each weekday into the directory given by
+#' \code{output}. Just open the image for each day of the week to
+#' find your shirt surprise.
 #'
 #' You can have as many shirts as you like in \file{rawShirts}, but
 #' you should probably have at least five or the sample will fail.
@@ -14,26 +14,35 @@
 #' This function also assumes your shirt images are JPG files, so
 #' probably just make sure they are JPG files.
 #'
+#' @param rawShirts file path to raw shirt files
+#' @param output file path for output files
+#'
 #' @return character vector of shirt order filenames
-shirtShuffler <- function() {
-    rawShirts <- list.files(path = "rawShirts", pattern = "[.]jpg$",
-                            full.names = TRUE)
+shuffleShirts <- function(rawShirts =
+                              system.file("rawShirts",
+                                          package = "shirtshufflr"),
+                          output = file.path(tempdir(), "shirtOrder")) {
+    
+    rawShirts <- list.files(
+        path = rawShirts,
+        pattern = "[.]jpg$",
+        full.names = TRUE)
+
     shirtOrder <- sample(x = seq_along(rawShirts), size = 5)
     shirtOrder <- rawShirts[shirtOrder]
 
     ## make sure output dir exists
-    if (!dir.exists("order"))
-        dir.create("order")
+    if (!dir.exists(output))
+        dir.create(path = output, recursive = TRUE)
     
     ## prepare new filenames
     newnames <- paste0(
         1:5, c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"),
         ".jpg")
-    newnames <- file.path("order", newnames)
+    newnames <- file.path(output, newnames)
 
     ## make it so
     file.copy(from = shirtOrder, to = newnames, overwrite = TRUE)
-    list.files(path = "order", full.names = TRUE)
-}
+    list.files(path = output, full.names = TRUE)
 
-shirtShuffler()
+}
